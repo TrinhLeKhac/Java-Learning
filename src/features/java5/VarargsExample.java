bf01f922cdae8c9a3af544fe56ab17f239f10e8e
TrinhLeKhac <emperorfire52@gmail.com>
2024-07-13 23:34:42 +0700

    features: VarargsExample

--- src/features/java5/VarargsExample.java
+++ src/features/java5/VarargsExample.java
@@ -1,2 +1,20 @@
-package features.java5;public class VarargsExample {
+package features.java5;
+
+// The varargs allows the method to accept zero or multiple arguments
+// If we don't know how many argument we will have to pass in the method, varargs is the best approach
+// Variable argument (varargs) must be the last argument.
+
+public class VarargsExample {
+    static void display(int num, String... values) {
+        System.out.println("number is: " + num);
+        for(String s: values) {
+            System.out.print(s + " ");
+        }
+        System.out.print('\n');
+    }
+    public static void main(String[] args) {
+        display(100);
+        display(500, "hello");
+        display(1000, "this", "is", "an", "example", "of", "varargs");
+    }
 }
