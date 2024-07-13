fcf9c2865b2983a4bfbdbea1237bac6f54ea6515
TrinhLeKhac <emperorfire52@gmail.com>
2024-07-13 19:20:20 +0700

    features: Add SAM interfaces definition and some pre-defined SAM interfaces

--- src/features/java8/FunctionalInterfaceExample.java
+++ src/features/java8/FunctionalInterfaceExample.java
@@ -1,2 +1,31 @@
-package features.java8;public class FunctionalInterfaceExample {
+package features.java8;
+
+// Functional Interface is an Interface that contains exactly one abstract method
+// It can have any number of default, static methods
+// It is also known as Single Abstract Method Interfaces or SAM Interfaces
+// It helps to achieve functional programming approach
+
+// Predefined-Function Interfaces
+
+// Consumer<T>: It represents an operations that accepts one input argument and returns no result
+// DoubleConsumer
+
+// BiConsumer<T,U>: two input arguments and no result
+
+// Function<T, R>: one input and a result
+// BiFunction<T,U,R>
+// DoubleFunction<R>: double type input and produces a result(type R)
+
+// Predicate<T>: boolean-valued function of one argument
+// BiPredicate<T,U>
+
+// BinaryOperator<T>: two operands of the same type and return a results of the same type
+// DoubleBinaryOperator: type double
+
+
+
+public class FunctionalInterfaceExample {
+    public static void main(String[] args) {
+
+    }
 }
