58a4e2e03ffc3c48b61e4ab6cced513024b696e3
TrinhLeKhac <emperorfire52@gmail.com>
2024-07-20 23:20:36 +0700

    collection: Add OuterClass

--- src/stream/IOStreamExample.java
+++ src/stream/IOStreamExample.java
@@ -32,5 +32,17 @@ package stream;
 // ois.close()
 // fis.close()
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public class IOStreamExample {
+    public static void main(String[] args) {
+        Stream.of(1, 2, 3, 4, 5, 6)
+        .filter(x -> x % 2 == 0)
+        .map(x -> x * 2)
+        .forEach(System.out::println);
+    }
+
+
 }
